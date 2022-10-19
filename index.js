4435803fdcc524e6cada294d5583a14efd276e0d
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-19 18:21:57 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = (array) => array.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return (fare) => integer * fare;
+}
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(array, func) {
+    return func(array);
+}
+
+
